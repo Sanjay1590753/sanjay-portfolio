70a5d54db1eb86dffd9f3082b677d15d8d136b65
Sanjay1590753 <Sanjay1590753@gmail.com>
2026-05-30 12:08:13 -0400

    Initial React Portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Sanjay Portfolio</h1>
+      <p>Technical Support Analyst | Aspiring Software Developer</p>
+    </main>
+  );
+}
+
+export default App;
